1b0d4446b769b98647d7a2c922e42414ec5a0d46
Altin Zhuta <altin.zhuta@deutschebahn.com>
2022-09-07 09:34:31 +0200

    popup message edited

--- vars.js
+++ vars.js
@@ -1,4 +1,4 @@
-export var programMsg = "Möchtest du die DB Lernwelt aufrufen?";
+export var programMsg = "Möchtest du die Aufgabe starten?";
 export var urlProgram = "https://dblernwelt.service.deutschebahn.com/";
 export var programZone = "program";
 export var programPopUp = "popUpProgram";
